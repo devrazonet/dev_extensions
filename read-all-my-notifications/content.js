de4c23a2507087373c2307d89e5944ec5e009ce6
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-09-21 16:26:35 -0300

    refactor: update layout from extension and added alert to quantaty tickets checked

--- read-all-my-notifications/content.js
+++ read-all-my-notifications/content.js
@@ -1,9 +1,11 @@
 var elements = document.querySelectorAll("#user-notifications #datatable-buttons > tbody > tr.unread > td:last-child > a");
 
-if(elements.length === 0) {
-  alert("Nenhuma notificação para ser lida nesta aba.")
-}
-
 for (var i = 0; i < elements.length; i++) {
   elements[i].click();
+}
+
+if(elements.length === 0) {
+  alert("Nenhuma notificação para ser lida nesta aba.")
+}else{
+  alert(`${elements.length} tickets marcados como lido nesta aba.`)
 }
