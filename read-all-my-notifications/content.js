e67bb3d06e4d407e8448ce05fa1b9f31ca681cc3
Luan Tedesco <luanfelipe2040@gmail.com>
2024-05-20 10:54:57 -0300

    added delay to finish notifications

--- read-all-my-notifications/content.js
+++ read-all-my-notifications/content.js
@@ -1,11 +1,18 @@
-var elements = document.querySelectorAll("#user-notifications #datatable-buttons > tbody > tr.unread > td:last-child > a");
+var elements = document.querySelectorAll(
+  '#user-notifications #datatable-buttons > tbody > tr.unread > td:last-child > a'
+)
 
-for (var i = 0; i < elements.length; i++) {
-  elements[i].click();
+async function clickWithDelay() {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].click()
+    await new Promise(resolve => setTimeout(resolve, 500))
+  }
+
+  if (elements.length === 0) {
+    alert('Nenhuma notificação para ser lida nesta aba.')
+  } else {
+    alert(`${elements.length} tickets marcados como lido nesta aba.`)
+  }
 }
 
-if(elements.length === 0) {
-  alert("Nenhuma notificação para ser lida nesta aba.")
-}else{
-  alert(`${elements.length} tickets marcados como lido nesta aba.`)
-}
+clickWithDelay()
